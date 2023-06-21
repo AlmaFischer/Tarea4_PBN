6658847b885e68bd14770054b77151d3ace3c3b3
Alan <amfischer@miuandes.cl>
2023-06-20 21:04:56 -0400

    Falta_poco

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from parabolico import *
 s=Simulador(9.81)
 obj1 = Objeto(5,10,0,5,45,1)
-obj2 = Objeto(12,1,10,10,1,-1)
+obj2 = Objeto(12,1,10,10,30,-1)
 r=s.run([obj1,obj2])
 c=s.collide(obj1,obj2)
 d=s.distance(obj1)
